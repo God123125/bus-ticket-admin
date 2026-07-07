20c0251596a439e4bf46066cc04665d0582bd755
DESKTOP-9RJEA40\Srun Kerry <srunkerry@gmail.com>
2026-07-07 14:19:50 +0700

    change schematic and setup ngx translate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/container/container.component').then((c) => c.Container),
+    children: [],
+  },
+];
