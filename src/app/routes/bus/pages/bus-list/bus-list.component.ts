2ac27be74af37a23dc995fcb88d97c318b48a263
DESKTOP-9RJEA40\Srun Kerry <srunkerry@gmail.com>
2026-07-08 15:15:11 +0700

    design table

--- src/app/routes/bus/pages/bus-list/bus-list.component.ts
+++ src/app/routes/bus/pages/bus-list/bus-list.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-bus-list',
-  imports: [],
+  imports: [MatFormFieldModule, MatButtonModule, TranslatePipe, MatIconModule, MatInputModule],
   templateUrl: './bus-list.component.html',
   styleUrl: './bus-list.component.scss',
 })
-export class BusListComponent {}
+export class BusListComponent {
+  constructor() {}
+}
